e942e1edbf50e090b9758e8383a3d1f75eae6287
PinLeGame <pinlegame@126.com>
2016-05-16 15:49:40 +0800

    添加定位数据

---
+++ frameworks/runtime-src/proj.ios_mac/PinLe_ platform.cpp
@@ -0,0 +1,9 @@
+//
+//  PinLe_ platform.cpp
+//  pinlegame
+//
+//  Created by Kennedy on 16/5/16.
+//
+//
+
+#include "PinLe_ platform.hpp"
---
+++ frameworks/runtime-src/proj.ios_mac/PinLe_ platform.hpp
@@ -0,0 +1,14 @@
+//
+//  PinLe_ platform.hpp
+//  pinlegame
+//
+//  Created by Kennedy on 16/5/16.
+//
+//
+
+#ifndef PinLe__platform_hpp
+#define PinLe__platform_hpp
+
+#include <stdio.h>
+
+#endif /* PinLe__platform_hpp */
