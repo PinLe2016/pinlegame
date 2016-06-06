d9168bb89adb9b55907355811bc13792533fbca8
PinLeGame <pinlegame@126.com>
2016-06-06 16:54:32 +0800

    奖池接口联调

--- src/app/model/Server/ServerJackpot.lua
+++ src/app/model/Server/ServerJackpot.lua
@@ -46,3 +46,51 @@ function Server:getgoldspooladlist_callback()
     NotificationCenter:Instance():PostNotification(G_NOTIFICATION_EVENT.JACKPOTLIST_INFOR_POST)
    
 end
+
+--3.6.3获取指定金币奖池详情(命令：getgoldspoolbyid)
+--goldspoolid 是 金币奖池ID  String  GUID
+function Server:getgoldspoolbyid(activityid)
+       local _params ={}
+       _params={
+            goldspoolid=activityid
+   }
+   
+    self:request_http("getgoldspoolbyid" , _params ); 
+end
+
+
+function Server:getgoldspoolbyid_callback()
+     dump(self.data)
+    if self.data.err_code~=0  then
+        self:show_float_message("获取奖池专区列表失败:" .. self.data.err_msg)
+        return
+    end
+    -- LocalData:Instance():set_getgoldspoollistbale(self.data)--保存数据
+    -- NotificationCenter:Instance():PostNotification(G_NOTIFICATION_EVENT.JACKPOTLIST_INFOR_POST)
+   
+end
+
+--3.6.4 获取最近10次金币奖池金币奖励接口(getrecentgoldslist)
+--count 是 获得多少个 int
+--注释，接口暂时未完成
+function Server:getrecentgoldslist(count)
+       local _params ={}
+       _params={
+            count=count
+   }
+   
+    self:request_http("getrecentgoldslist" , _params ); 
+end
+
+
+function Server:getrecentgoldslist_callback()
+     dump(self.data)
+    if self.data.err_code~=0  then
+        self:show_float_message("获取最近10次金币奖池金币奖励失败:" .. self.data.err_msg)
+        return
+    end
+    -- LocalData:Instance():set_getgoldspoollistbale(self.data)--保存数据
+    -- NotificationCenter:Instance():PostNotification(G_NOTIFICATION_EVENT.JACKPOTLIST_INFOR_POST)
+   
+end
+
