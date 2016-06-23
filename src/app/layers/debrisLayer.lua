5c335d08465cea057080397ac9174320f7c2abd7
PinLeGame <pinlegame@126.com>
2016-06-23 19:13:03 +0800

    显示BUG

--- src/app/layers/debrisLayer.lua
+++ src/app/layers/debrisLayer.lua
@@ -51,12 +51,10 @@ function debrisLayer:refresh_table()
 
     local pos_x, pos_y =self.point.x,self.point.y
     local row ,col =self.row,self.col 
+    local path=cc.FileUtils:getInstance():getWritablePath()
    for i=1,row do
         for j=1,col do
-                local fragment_sprite = display.newScale9Sprite(self.filename, 0,0, cc.size(self._size.width,self._size.height))
-                if not fragment_sprite then
-                    fragment_sprite=display.newScale9Sprite("httpwww.pinlegame.comGameImageaffbd109-a341-4f1c-8b3f-edb581f01c68.jpg", 0,0, cc.size(self._size.width,self._size.height))
-                end
+                local fragment_sprite = display.newScale9Sprite(path..self.filename, 0,0, cc.size(self._size.width,self._size.height))
                 fragment_sprite:setAnchorPoint(0, 0)
                 local po = self._size
                 local rect = cc.rect(0,0, po.width/row-3, po.height/col-3)
