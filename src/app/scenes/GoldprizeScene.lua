aef4516b16ad9f2259025247b89e6a817afd5304
PinLeGame <pinlegame@126.com>
2016-07-18 11:11:26 +0800

    奖池

--- src/app/scenes/GoldprizeScene.lua
+++ src/app/scenes/GoldprizeScene.lua
@@ -79,7 +79,7 @@ function GoldprizeScene:data_init(  )
 
 						local tag=sender:getTag()
 
-						local jackpotlayer= jackpotlayer.new({id=jac_data[2*i-1]["id"],  adownerid= jac_data[2*i-1]["adownerid"] })
+						local jackpotlayer= jackpotlayer.new({id=jac_data[2*i-1]["id"],  adownerid= jac_data[2*i-1]["adownerid"],goldspoolcount=  jac_data[2*i]["goldspoolcount"] })
 
 						self:addChild(jackpotlayer)
 
