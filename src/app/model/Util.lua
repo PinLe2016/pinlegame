8ffabf26d36d5c94db3acc51388102c2afc6e9a9
PinLeGame <pinlegame@126.com>
2016-08-04 17:15:46 +0800

    配置文件重新添加

---
+++ src/app/model/Util.lua
@@ -0,0 +1,335 @@
+Util = {}
+
+function Util:CenterNodeInParent(node, use_boundbox)
+   local parent = node:getParent()
+   local size = parent:getContentSize()
+   if (use_boundbox) then
+      size = parent:getCascadeBoundingBox()
+   end
+   node:setPosition(size.width / 2, size.height / 2)
+end
+
+
+
+
+-- 除头尾空格
+function Util:trim (s)
+	return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
+end
+
+-- 除头尾空格
+function Util:sub_str (s,parm,parm1)
+  local str=string.gsub(s, parm, "")
+  return string.gsub(str, parm1, "")
+end
+--拆分字符串返回表
+function Util:lua_string_split(str, split_char)      
+ local sub_str_tab = {};  
+ while (true) do          
+ local pos = string.find(str, split_char);    
+ if (not pos) then              
+  local size_t = table.getn(sub_str_tab)  
+  table.insert(sub_str_tab,size_t+1,str);  
+  break;    
+ end  
+   
+ local sub_str = string.sub(str, 1, pos - 1);                
+ local size_t = table.getn(sub_str_tab)  
+ table.insert(sub_str_tab,size_t+1,sub_str);  
+ local t = string.len(str);  
+ str = string.sub(str, pos + 1, t);     
+ end      
+ return sub_str_tab;  
+end  
+
+
+
+function Util:FormatTime(orginSecond)
+   local d = orginSecond
+   local h = math.floor(d/3600)
+   local m = (d - h * 3600) / 60
+   local s = (m - math.floor(m)) *60
+   local hour = math.floor(h)
+   if (hour < 10) then
+	  hour = '0' .. hour
+   end
+   local minutes = math.floor(m)
+   if (math.abs(minutes) < 10) then
+	  minutes= '0' .. math.abs(minutes)
+   end
+   local second = math.floor(s)
+   if (math.abs(second) < 10) then
+	  second = '0' .. math.abs(second)
+   end
+   return string.format("%sh%sm%ss", hour, minutes, second)
+end
+
+-- 冒号格式时间显示
+function Util:FormatTime_colon(orginSecond)
+
+   local d = orginSecond
+   local day  = math.floor(d/60/60/24)
+   local h = math.floor((d/60/60)%24)
+   local m = (d/60)%60
+   local s = d%60
+   local hour = math.floor(h)
+   if (hour < 10) then
+    hour = '0' .. hour
+   end
+   local minutes = math.floor(m)
+   if (math.abs(minutes) < 10) then
+    minutes= '0' .. math.abs(minutes)
+   end
+   local second = math.floor(s)
+   if (math.abs(second) < 10) then
+    second = '0' .. math.abs(second)
+   end
+    local _table={day.."天",hour.."小时",minutes.."分",second.."秒"}
+    -- dump(_table)
+   return _table--string.format("%s:%s:%s", hour, minutes, second)
+end
+
+-- 字符串拆成字符（中文）
+function Util:UTF8ToCharArray(str)
+   -- local UTF8ToCharArray = function(str)
+   local charArray = {};
+   local iStart = 0;
+   local strLen = str:len();
+
+   local function bit(b)
+     return 2 ^ (b - 1);
+   end
+
+   local function hasbit(w, b)
+     return w % (b + b) >= b;
+   end
+
+   local checkMultiByte = function(i)
+     if (iStart ~= 0) then
+         charArray[#charArray + 1] = str:sub(iStart, i - 1);
+         iStart = 0;
+     end        
+   end
+    
+   for i = 1, strLen do
+     local b = str:byte(i);
+     local multiStart = hasbit(b, bit(7)) and hasbit(b, bit(8));
+     local multiTrail = not hasbit(b, bit(7)) and hasbit(b, bit(8));
+
+     if (multiStart) then
+         checkMultiByte(i);
+         iStart = i;
+         
+     elseif (not multiTrail) then
+         checkMultiByte(i);
+         charArray[#charArray + 1] = str:sub(i, i);
+     end
+   end
+
+   -- process if last character is multi-byte
+   checkMultiByte(strLen + 1);
+
+   return charArray;
+end
+
+-- 判断是否有敏感词
+function Util:isExistSensitiveWord(str)
+  local sensitive_csv = LocalConfig:Instance():get_table("sensitiveWord")
+  for i,v in ipairs(sensitive_csv) do
+    local begin_, end_ = string.find(str, v.mask_word)
+    if begin_ ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
+-- 替换敏感词
+function Util:filterSensitiveWord(str)
+  local sensitive_csv = LocalConfig:Instance():get_table("sensitiveWord")
+  local text = str
+  for i,v in ipairs(sensitive_csv) do
+    text = string.gsub(text, v.mask_word, "**")
+  end
+  return text
+end
+
+
+
+function Util:removeDirectory(path)
+   local path = cc.FileUtils:getInstance():getWritablePath() .. path .. "/"
+   cc.FileUtils:getInstance():removeDirectory(path)
+end
+
+
+--比较table
+function Util:compareTables(table1 , table2)
+    if type(table1)~="table"   or  type(table2)~="table"  then 
+      printError("cant compare noTable value") 
+      return nil
+    end 
+
+    if table1==table2 then 
+      printError("table1 and table2 are the same refercence")
+      return nil
+    end
+
+
+    local keys1=table.keys(table1)
+    local keys2=table.keys(table2)
+
+    if (#keys1) ~= (#keys2) then 
+      return false 
+    end 
+
+    for k1,v1 in pairs(table1) do 
+        
+        local v2=table2[k1]
+        if v2==nil then 
+          return false 
+        end 
+
+        if type(v1)~=type(v2) then 
+          return false 
+        end
+
+        if type(v1)=="function"  or type(v1)=="userdata" or type(v1)=="thread"   then 
+          printError("sorry ,we cant compare tables include function , userdata or thread")
+          return nil
+        end
+
+        if type(v1)=="table" then 
+              local ret=Util:compareTables(v1,v2)
+              if ret==false then 
+                return false 
+              end
+        else 
+              if v1~=v2 then 
+                return false 
+              end     
+        end
+
+   end
+
+   return true
+
+end
+
+
+function Util:dumpTexture()
+        print("\n___________________TextureData__________________________\n")
+        print(cc.Director:getInstance():getTextureCache():getCachedTextureInfo())
+        print("\n___________________TextureData__________________________\n")
+end
+
+function Util:scene_control(scene)
+        local str_scene="app.scenes."..scene
+        display.replaceScene(require(str_scene):new())
+end
+function Util:scene_controlid(scene,params)
+        local str_scene="app.scenes."..scene
+        display.replaceScene(require(str_scene).new(params))-- （.与：区别）
+end
+
+
+
+function Util:tableLength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
+--随机数
+function Util:rand(  )
+       return math.random(1000,9999)
+end
+function Util:getdays(  )
+            local year,month = os.date("%Y", os.time()), os.date("%m", os.time())+1 -- 正常是获取服务器给的时间来算
+            local dayAmount = os.date("%d", os.time({year=year, month=month, day=0})) -- 获取当月天数
+            return  dayAmount
+end
+
+
+
+--分享相关
+--截屏功能
+function Util:captureScreen()
+    local file=cc.FileUtils:getInstance():getWritablePath().."screenshoot.jpg"
+
+    if device.platform=="ios" then
+      print("我累的去去")
+      file=cc.FileUtils:getInstance():getWritablePath().."res/screenshoot.jpg"
+    end
+      display.captureScreen( function (bSuc, filePath)
+                    --bSuc 截屏是否成功
+                      --filePath 文件保存所在的绝对路径
+                end, file)
+      return file
+end
+
+--分享功能
+function Util:share()
+  Util:captureScreen()
+
+  local file=cc.FileUtils:getInstance():getWritablePath().."screenshoot.jpg"
+
+  if device.platform=="ios" then
+      file="res/screenshoot.jpg"
+  end
+
+   local login_info=LocalData:Instance():get_user_data()
+   
+   local share=cc.UM_Share:createWithShare(file,login_info["playerid"])
+   share:addTo(display.getRunningScene(),1000)
+end
+
+
+-- --读取json 文件
+function Util:read_json(path_json)
+
+  local file_path=path_json
+ 
+  if device.platform=="mac" then
+    file_path=cc.FileUtils:getInstance():getWritablePath()..path_json
+  end
+  local fileStr = cc.HelperFunc:getFileData(file_path)
+
+  local fileData = json.decode(fileStr)
+  return fileData
+
+end
+
+--去除json 括号
+function Util:remove_json_str(pathname)
+  string.gsub(pathname, "%b()", "")
+end
+
+--日期转成时间戳
+function Util:dateTotimestamp(birthday)
+  local secondOfToday = os.time({day=birthday.day, month=birthday.month,
+    year=birthday.year, hour=1, minute=0, second=0})
+
+  dump(secondOfToday)
+  return secondOfToday
+end
+--音乐
+function Util:player_music(musicname,cycle ) --音乐名字，是否重播，是否设置禁止播放
+
+    if LocalData:Instance():get_music() then
+      audio.playMusic(G_SOUND[musicname],cycle)
+    else
+      print("抱歉无法播放音乐")
+   end   
+end
+function Util:stop_music( musicname ) -- 停止播放音乐
+    
+     if not LocalData:Instance():get_music() then
+       audio.stopMusic(G_SOUND[musicname])
+    else
+      print("抱歉无法关闭音乐")
+   end   
+end
+
+
+return Util
+
+
---
+++ src/config.lua
@@ -0,0 +1,153 @@
+
+-- 0 - disable debug info, 1 - less debug info, 2 - verbose debug info
+DEBUG = 2
+
+-- display FPS stats on screen
+DEBUG_FPS = false
+
+-- dump memory info every 10 seconds
+DEBUG_MEM = false
+
+-- load deprecated API
+LOAD_DEPRECATED_API = false
+
+-- load shortcodes API
+LOAD_SHORTCODES_API = true
+
+-- screen orientation
+CONFIG_SCREEN_ORIENTATION = "portrait"
+
+-- design resolution
+CONFIG_SCREEN_WIDTH  = 640
+CONFIG_SCREEN_HEIGHT = 960
+
+-- auto scale mode
+CONFIG_SCREEN_AUTOSCALE = "FIXED_WIDTH"
+
+--传输加密Key
+MD5_KEY="PINLEGAME"
+--版本号
+PINLE_VERSION="3.0.0"
+
+--公共
+ qqqq=0
+ dialogdetermine=0  --弹出提示框
+
+--测试版本还是正式版本标记
+IS_RELEASE=true --false 测试版 ，true 正式版
+
+--四个公有文件引入
+require("app.model.Server.Server")--请求文件
+require("app.model.NotificationCenter")--消息文件
+require("app.model.LocalData.LocalData")--数据文件
+DetailsLayer = require("app.layers.DetailsLayer")--活动详情
+ContrastRecordLayer = require("app.layers.ContrastRecordLayer")--对比积分排行榜
+SurpriseOverScene = require("app.scenes.SurpriseOverScene")--惊喜吧结束界面
+OnerecordLayer = require("app.layers.OnerecordLayer")--个人积分界面
+RankinglistofactiviesLayer = require("app.layers.RankinglistofactiviesLayer")--排行榜界面
+FloatingLayerEx = require("app.layers.FloatingLayer")--提示文件
+GameScene = require("app.scenes.GameScene")
+MainInterfaceScene = require("app.scenes.MainInterfaceScene")--主界面
+SurpriseScene = require("app.scenes.SurpriseScene")--惊喜吧
+
+
+
+
+--消息处理方法枚举
+G_NOTIFICATION_EVENT = {
+	LOGIN_POST=							"login_post",--登陆
+	REG=								"REG",--注册
+	SURPRIS_SCENE=						"SURPRIS_SCENE",--惊喜吧场景
+	SURPRIS_LIST=						"SURPRIS_LIST", --获取惊喜吧列表
+	SURPRIS_LIST_IMAGE=					"SURPRIS_LIST_IMAGE",--惊喜吧图片下载
+	DETAILS_LAYER_IMAGE=				"DETAILS_LAYER_IMAGE", --活动详情
+	RANK_LAYER_IMAGE   =				"RANK_LAYER_IMAGE" ,--排行榜
+	ONERECORD_LAYER_IMAGE=				"ONERECORD_LAYER_IMAGE" ,--个人记录排行榜
+	CONTRASRECORD_LAYER_IMAGE=			"CONTRASRECORD_LAYER_IMAGE" ,--对比积分排行榜
+	LAOHUJI_LAYER_IMAGE=				"LAOHUJI_LAYER_IMAGE" ,--老虎机
+	USERINFOINIT_LAYER_IMAGE=			"USERINFOINIT_LAYER_IMAGE", --个人信息初始化数据
+	USERINFO_LAYER_IMAGE=				"USERINFO_LAYER_IMAGE", --个人信息修改
+	ACTIVITYYADLIST_LAYER_IMAGE=		"ACTIVITYYADLIST_LAYER_IMAGE", --获取指定活动的广告列表 
+	ACTIVITYYADLISTPIC_LAYER_IMAGE=		"ACTIVITYYADLISTPIC_LAYER_IMAGE", --下载指定活动的广告列表 
+	JACKPOTLIST_POST=					"JACKPOTLIST_POST", --下载指定奖池的广告列表 
+	JACKPOTLIST_PIC_POST=				"JACKPOTLIST_PIC_POST", --下载指定奖池的广告列表图片 
+	JACKPOTLIST_INFOR_POST=				"JACKPOTLIST_INFOR_POST", --获取金币奖池的广告列表
+	JACKPOTLISTPIC_INFOR_POST=			"JACKPOTLISTPIC_INFOR_POST", --获取金币奖池的广告列表
+	GOLDSPOOLBYID_POST=					"CHECKINHISTORY_POST", --   获取指定金币奖池详情  getrecentgoldslist
+	RECENTGOLDSLIST_POST=				"RECENTGOLDSLIST_POST", --获取最近10次金币奖池金币奖励接口  getrecentgoldslist
+	CHECK_POST=							"CHECK_POST", --签到
+	GAMERECORD_POST=					"GAMERECORD_POST", --拼图结束后上传数据  set_getcheckinhistory
+	CHECKINHISTORY_POST=				"CHECKINHISTORY_POST", --签到历史   
+	FRIENDLIST_POST=					"FRIENDLIST_POST", --查询好友列表  getfriendlist 
+	POOL_RANDOM_GOLDS=					"poolrandomgolds",--奖池劲舞团每次随机获取金币信息 getgoldspoolrandomgolds
+	ACTIVITYCODE=						"ACTIVITYCODE",--活动吗  Activity code
+	VERRSION=							"VERRSION",--活动吗  热更新消息
+	REGISTRATIONCODE=					"REGISTRATIONCODE",--注册的验证码
+	PASSWOEDCHANGE=					"PASSWOEDCHANGE",--注册的验证码
+	WINNERS=					            "WINNERS",--获奖名单 
+	INVITATION_POLITE=					            "INVITATION_POLITE",--邀请有礼
+	FRIENDSLEVELUP=					            "FRIENDSLEVELUP",--一键领取
+	BACKSUPPOR=					            "BACKSUPPOR",--参与卷返回
+	STECODE=								"setinvitecode",--邀请码刷新
+	EMAILADDRESS=							"EMAILADDRESS",--初始化邮件地址
+	JIGSAWCOUNT=							"JIGSAWCOUNT",--  点击弹出框确定
+	AUTOMATICPUZZLE=						"AUTOMATICPUZZLE",--  点击弹出框确定自动拼图
+	PRIZEPOOLDETAILS=						"PRIZEPOOLDETAILS"   --切换奖池详情
+}
+
+G_SOUND={
+	ACTIVITY="sound/background/activity.mp3",
+	GAMEBG="sound/background/gameBg.mp3",
+	MENUMUSIC="sound/background/menumusic.mp3",
+	PERSONALCHAGE="sound/background/personalchage.mp3",
+
+	--音效
+	COUNTDOWN="sound/effect/321.mp3",
+	BACK="sound/effect/back.mp3",
+	BUTTON="sound/effect/button.mp3",
+	CANCEL="sound/effect/cancel.mp3",
+	DROPPIC="sound/effect/droppic.mp3",
+	ERROR="sound/effect/error.mp3",
+	FALLMONEY="sound/effect/fallmoney.mp3",
+	LOGO="sound/effect/logo.mp3",
+	LOST="sound/effect/lost.mp3",
+	MOVE="sound/effect/move.mp3",
+	PAGECHANGE="sound/effect/pagechange.mp3",
+	PAGEPLAY="sound/effect/pageplay.mp3",
+	PREVIEW="sound/effect/preview.mp3",
+	SETTING="sound/effect/setting.mp3",
+	WIN="sound/effect/win.mp3",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
