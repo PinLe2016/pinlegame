fbfd41ecb4c02f299a6385ba89bfb632024a3eb5
liuyali000312 <807476512@qq.com>
2016-12-23 12:35:53 +0800

    光标

--- src/app/layers/ChangepasswordLayer.lua
+++ src/app/layers/ChangepasswordLayer.lua
@@ -33,9 +33,41 @@ function ChangepasswordLayer:init(  )
        self._bt:addTouchEventListener(function(sender, eventType  )
           self:touch_callback(sender, eventType)
       end)
-       self._oldpassword=self.changepassword:getChildByTag(78)   --   旧密码
-       self._password=self.changepassword:getChildByTag(79)   --   新密码
-       self.again_password=self.changepassword:getChildByTag(80)   --   再次新密码
+       self._oldpassword1=self.changepassword:getChildByTag(78)   --   旧密码
+       self._password2=self.changepassword:getChildByTag(79)   --   新密码
+       self.again_password3=self.changepassword:getChildByTag(80)   --   再次新密码
+      self._oldpassword1:setVisible(false)
+      self._password2:setVisible(false)
+      self.again_password3:setVisible(false)
+      local res = "  "--res/png/DLkuang.png"
+      local width = 200
+      local height = 27
+
+      self._oldpassword = ccui.EditBox:create(cc.size(width,height),res)
+      self.changepassword:addChild(self._oldpassword )
+      self._oldpassword :setPosition(cc.p(self._oldpassword1:getPositionX(),self._oldpassword1:getPositionY()))--( cc.p(130,380 ))  
+      self._oldpassword :setPlaceHolder("请输入原密码")
+      self._oldpassword :setAnchorPoint(0.5,0.6)  
+      self._oldpassword :setMaxLength(18)
+      self._oldpassword :setInputFlag(cc.EDITBOX_INPUT_FLAG_PASSWORD)
+
+      self._password = ccui.EditBox:create(cc.size(width,height),res)
+      self.changepassword:addChild(self._password )
+      self._password :setPosition(cc.p(self._password2:getPositionX(),self._password2:getPositionY()))--( cc.p(130,380 ))  
+      self._password :setPlaceHolder("请输入新密码")
+      self._password :setAnchorPoint(0.5,0.6)  
+      self._password :setMaxLength(18)
+      self._password :setInputFlag(cc.EDITBOX_INPUT_FLAG_PASSWORD)
+
+
+      self.again_password = ccui.EditBox:create(cc.size(width,height),res)
+      self.changepassword:addChild(self.again_password )
+      self.again_password :setPosition(cc.p(self.again_password3:getPositionX(),self.again_password3:getPositionY()))--( cc.p(130,380 ))  
+      self.again_password :setPlaceHolder("再次输入新密码")
+      self.again_password :setAnchorPoint(0.5,0.6)  
+      self.again_password :setMaxLength(18)
+      self.again_password :setInputFlag(cc.EDITBOX_INPUT_FLAG_PASSWORD)
+
 
 
 end
@@ -54,9 +86,9 @@ function ChangepasswordLayer:touch_callback(sender, eventType)
          
 end
 function ChangepasswordLayer:pass( )
-                  local _oldpassword=self._oldpassword:getString()     
-                  local _password=self._password:getString()
-                  local again_password=self.again_password:getString()   
+                  local _oldpassword=self._oldpassword:getText()--getString()     
+                  local _password=self._password:getText()--getString()
+                  local again_password=self.again_password:getText()--getString()   
                   print("修改密码",_oldpassword," ",_password," ",again_password)
                   if _password  ~= again_password    then
                       Server:Instance():prompt("两次密码输入不一样")
