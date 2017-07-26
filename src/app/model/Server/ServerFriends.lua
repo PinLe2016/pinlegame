5e111d33bb67396228a068506ecc49cb7319721f
liuyali000312 <807476512@qq.com>
2017-07-26 17:02:51 +0800

    x修改问题

--- src/app/model/Server/ServerFriends.lua
+++ src/app/model/Server/ServerFriends.lua
@@ -261,7 +261,7 @@ end
 
 
 function Server:getfriendhelplist_callback()
-        dump(self.data)
+        --dump(self.data)
     if self.data.err_code~=0  then
         self:show_float_message( self.data.err_msg)
         return
